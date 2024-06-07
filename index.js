08990cb3775471fd5120879cc72d70645e671ae3
claudia <claudianataliurena@gmail.com>
2024-06-07 00:09:25 -0300

    subiendo get

--- index.js
+++ index.js
@@ -2,12 +2,50 @@ const express = require("express");
 const app = express();
 
 const bodyParser = require("body-parser");
-
+const mysql = require("mysql2");
 const cors = require("cors");
+
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "crud_contact",
+});
+
+// const db = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "password",
+//   database: "crud_contact",
+// });
+
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 console.log("hola mundo");
 
-app.listen(4000, () => { console.log("corriendo en el puerto 4000")});
+app.listen(4000, () => {
+  console.log("corriendo en el puerto 4000");
+});
+
+app.get("/api/get", (req, res) => {
+
+    const sqlGet = "SELECT * FROM contact_db";
+    db.query(sqlGet,(error,result)=>{
+        res.send(result);
+
+    });
+})
+
+app.get("/", (req, res) => {
+//   const sqlInsert =
+//     "INSERT INTO crud_contact.contact_db (name,email,contact) VALUES ('johndoe','johndoe@gmail.com',3123456)";
+
+   
+//   db.query(sqlInsert, (err, result) => {
+//     console.log("error", err);
+//     console.log("result", result);
+//     res.send("hello express");
+// });
+});
